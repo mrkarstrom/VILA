c76632bf5efff342bfb62877ec7fd938ae6d0df0
Magnus Karström <mrkarstrom81@gmail.com>
2025-01-14 13:49:52 +0100

    feat: Folder structure

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 
+app.use('api/v1', apiRouter);
 
-app.listen(PORT, () => {
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
-})
-
+  });
+}
 
 export default app;
